a84fe8da04cb65702764f4427a38564e764e992f
muhammedshebinva <muhammedshebinva@gmail.com>
2022-02-05 21:36:45 +0530

    src/SwipeButtons.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import Header from './Header';
-import TinderCards from './TiderCards'
+import TinderCards from './TiderCards';
+import SwipeButtons from './SwipeButtons';
+
 function App() {
   return (
     <div className="App">
       
         <Header/>
         <TinderCards />
+        <SwipeButtons />
 
     </div>
   );
